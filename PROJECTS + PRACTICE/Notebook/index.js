e8f6c3c4134e83b2fa0a74d275b1277e35355617
Ansh Thakur <mailtoanshthakur@gmail.com>
2024-05-27 11:55:47 -0700

    ~JOD

--- PROJECTS + PRACTICE/Notebook/index.js
+++ PROJECTS + PRACTICE/Notebook/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs"); // This fs module is used to read the files in the directory
 
 // These two are working as a parsers
 app.use(express.json());
@@ -13,15 +14,30 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res, next) {
-  res.render("index.ejs"); //using render as ham inndex.ejs file(views) folder wali ko render kr rhe
-});
-app.get("/profile/:username", function (req, res, next) {
-  res.send("Working for any route"); // This is dynamic routing profile/: ke baad kuch bhi route de tab bhi hame output milega
+  fs.readdir(`./files`, function (err, files) {
+    console.log(files); // return [] on if files folder is empty on reloading the webpage otherwise the file name
+    res.render("index.ejs", { files: files }); //using render as ham inndex.ejs file(views) folder wali ko render kr rhe
+  });
 });
 
-app.get("/greet/:username/:age", function (req, res) {
-  res.send(`Welcome ${req.params.username} ${req.params.age}`); // ${req.params.username} -> direct username frontend se backend then backend se frontend pe gya
+app.post(`/create`, function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
 });
+
+// app.get("/profile/:username", function (req, res, next) {
+
+//   res.send("Working for any route"); // This is dynamic routing profile/: ke baad kuch bhi route de tab bhi hame output milega
+// });
+
+// app.get("/greet/:username/:age", function (req, res) {
+//   res.send(`Welcome ${req.params.username} ${req.params.age}`); // ${req.params.username} -> direct username frontend se backend then backend se frontend pe gya
+// });
 
 app.listen(3000, function () {
   console.log("Server running");
